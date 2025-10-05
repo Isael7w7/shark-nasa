be6cc69b0c8a1003a4438df376a3046f00201159
RaulBecerraB <ingenierobecerrabarcelo@gmail.com>
2025-10-04 20:15:55 -0600

    cambié el apiConfig

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,61 +1,13 @@
-// src/config/apiConfig.js (CÓDIGO COMPLETO)
-
-// La URL base del backend. 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://72ee35158b5d.ngrok-free.app'
 
 const endpoints = {
+    sharks: {
+        getAll: `${BASE_URL}/Sharks`
+    },
+    tracking: {
+        getBySharkId: (sharkId) => `${BASE_URL}/Tracking/shark/${sharkId}`,
+        predict: (sharkId, iterations = 1) => `${BASE_URL}/Tracking/shark/${sharkId}/predict?iterations=${iterations}`
+    }
+}
 
-  sharks: {
-    getAll: `${BASE_URL}/sharks/all`,
-
-    // getById: (id) => `${BASE_URL}/sharks/${id}`
-  },
-
-
-  categories: {
-    getAll: `${BASE_URL}/admin/categories/all`,
-    create: `${BASE_URL}/admin/categories/create`,
-    update: (id) => `${BASE_URL}/admin/categories/update/${id}`,
-    delete: (id) => `${BASE_URL}/admin/categories/delete/${id}`
-  },
-  subcategories: {
-    getAll: `${BASE_URL}/admin/subcategories/all`,
-    create: `${BASE_URL}/admin/subcategories/create`,
-    update: (id) => `${BASE_URL}/admin/subcategories/update/${id}`,
-    delete: (id) => `${BASE_URL}/admin/subcategories/delete/${id}`
-  },
-  materials: {
-    getAll: `${BASE_URL}/admin/materials/all`,
-    create: `${BASE_URL}/admin/materials/create`,
-    update: (id) => `${BASE_URL}/admin/materials/update/${id}`,
-    delete: (id) => `${BASE_URL}/admin/materials/delete/${id}`
-  },
-  roles: {
-    getAll: `${BASE_URL}/admin/role/all`,
-    create: `${BASE_URL}/admin/role/create`,
-    update: (id) => `${BASE_URL}/admin/role/update/${id}`,
-    delete: (id) => `${BASE_URL}/admin/role/delete/${id}`
-  },
-  users: {
-    getAll: `${BASE_URL}/admin/user/all`,
-    update: (id) => `${BASE_URL}/admin/user/${id}`,
-    getUserDetails: `${BASE_URL}/user/details`,
-  },
-  movements: {
-    getAll: `${BASE_URL}/admin/movement/all`,
-    create: `${BASE_URL}/admin/movement/create`
-  },
-  tokens: {
-    getUserId: `${BASE_URL}/tokens/userId`
-  },
-  borrowings: {
-    getAll: `${BASE_URL}/admin/borrow/all`,
-    create: `${BASE_URL}/admin/borrow/create`,
-    update: (id) => `${BASE_URL}/admin/borrow/update/${id}`
-  },
-
-  signUp: `${BASE_URL}/register`,
-  login: `${BASE_URL}/login`
-};
-
-export default endpoints;
+export default endpoints
